1adf58bad97381bac8f352a1ceeb1d6fd6fa0b33
andre odendaal <aodendaal.direct@gmail.com>
2019-04-20 23:49:43 -0700

    working version, pres submission

--- Project data Modelling with Postgress/etl.py
+++ Project data Modelling with Postgress/etl.py
@@ -7,56 +7,79 @@ from sql_queries import *
 
 def process_song_file(cur, filepath):
     # open song file
-    df = 
+    df = pd.read_json(filepath, lines=True)
+        
+    df_selected = df[['song_id', 'title', 'artist_id', 'year', 'duration']]
+    song_data_array = df_selected.values[0]
 
     # insert song record
-    song_data = 
-    cur.execute(song_table_insert, song_data)
+    song_data = song_data_array.tolist()
+    cur.execute(song_table_insert, song_data)    
     
     # insert artist record
-    artist_data = 
-    cur.execute(artist_table_insert, artist_data)
+    df_artist = df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']] 
+    artist_data_array = df_artist.values[0]
 
+    artist_data = artist_data_array.tolist()
+    cur.execute(artist_table_insert, artist_data)
+    
+    return    
+    
 
 def process_log_file(cur, filepath):
+    
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df[(df.page == "NextSong")]
 
     # convert timestamp column to datetime
-    t = 
+    #t = 
+    df["ts"] = pd.to_datetime(df["ts"])     
     
+     
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = [df.ts, df.ts.dt.time, df.ts.dt.hour, df.ts.dt.day, df.ts.dt.week, df.ts.dt.month, df.ts.dt.year, df.ts.dt.weekday]
+    column_labels = ['timestamp','time','hour', 'day', 'week of year', 'month', 'year', 'weekday']
+    time_dict = dict(zip(column_labels, time_data))
+    
+    time_df = time_df = pd.DataFrame(time_dict)
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[['userId', 'firstName', 'lastName' , 'gender', 'level']].drop_duplicates()
 
     # insert user records
     for i, row in user_df.iterrows():
         cur.execute(user_table_insert, row)
 
     # insert songplay records
-    for index, row in df.iterrows():
-        
+  
+    for i_key, row in df.iterrows():        
         # get songid and artistid from song and artist tables
-        results = cur.execute(song_select, (row.song, row.artist, row.length))
-        songid, artistid = results if results else None, None
-
+        cur.execute(song_select, (row.song, row.artist, row.length))
+        results = cur.fetchone()
+        
+        if results:
+            songid, artistid = results
+        else:
+            songid, artistid = None, None        
+ 
         # insert songplay record
-        songplay_data = 
+        songplay_data = (i_key, row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
-
+        
+    return
+        
 
 def process_data(cur, conn, filepath, func):
     # get all files matching extension from directory
+    
+    print(filepath, func)
+    
     all_files = []
     for root, dirs, files in os.walk(filepath):
         files = glob.glob(os.path.join(root,'*.json'))
@@ -69,9 +92,14 @@ def process_data(cur, conn, filepath, func):
 
     # iterate over files and process
     for i, datafile in enumerate(all_files, 1):
+        
         func(cur, datafile)
         conn.commit()
         print('{}/{} files processed.'.format(i, num_files))
+        
+    print('{}/{} files processed.'.format(i, num_files)) 
+    
+    return
 
 
 def main():
